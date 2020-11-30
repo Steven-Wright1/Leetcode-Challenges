c86a6d7e580aab089e3c6b2c55b648ec024bba93
Steven Wright <71319046+Steven-Wright1@users.noreply.github.com>
2020-11-30 10:29:30 +0000

    Add files via upload

---
+++ maximum69Number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        
+        string res = to_string(num);
+        
+        for(int i = 0; i < res.length(); i++){
+            if(res[i] == '6'){
+                res[i] = '9';
+               break;
+            }
+        }
+        
+       num = stoi(res);
+        
+        return num; 
+    }
+};
